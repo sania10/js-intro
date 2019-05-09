82ac52fa9286a8d29607687174f07efd00915449
sania10 <saniasimran98@gmail.com>
2019-05-09 15:17:51 +0530

    yoo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function marks(num){
+    if(num>=85)
+    {
+console.log("distiction");
+}
+else if(num<=85 && "num>=60") {
+console.log("fc");
+}
+else if(num<=60 && "num>=30") {
+console.log("sc");
+}
+else {
+console.log("fail");
+}
+}
+var num = prompt("enter the number");
+marks(num);
